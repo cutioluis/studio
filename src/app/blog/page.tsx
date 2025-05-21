5d466faaa919b6a830d2e7aecc4c62880f053485
Luis Cutiopala <cutioluis@gmail.com>
2025-05-21 13:00:35 -0500

    add: metricas al blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/components/ui/button';
 import { CalendarDays, ArrowRight } from 'lucide-react';
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
+import { Analytics } from '@vercel/analytics/next';
 
 export const metadata: Metadata = {
   title: 'Blog de Belleza y Emprendimiento | Ceci Glam',
@@ -83,6 +84,7 @@ export default function BlogPage() {
         )}
       </main>
       <Footer />
+      <Analytics />
     </div>
   );
 }
